83bcf37aebe26676631adf6b857a8731de378b38
Thibaut Girier <30323597+tgirier@users.noreply.github.com>
2020-05-12 11:13:34 +0200

    Added test for server connection

--- chat_test.go
+++ chat_test.go
@@ -1,3 +1,15 @@
 package chat_test
 
+func TestServerConn(t *testing.T) {
+	addr := ":8080"
+	errExpected := false
+
+	conn, err := chat.NewConn(addr)
+	defer conn.Close()
+
+	if errExpected != (err != nil) {
+		t.Errorf("NewConn(%s): error expected %t, got %v", addr, errExpected, err)
+	}
+}
+
 
