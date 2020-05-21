12ae4e47ebf65079b941b7ce0b48aebe38160f1b
Thibaut Girier <30323597+tgirier@users.noreply.github.com>
2020-05-21 12:20:08 +0200

    Added AtoB test message

--- chat_test.go
+++ chat_test.go
@@ -62,3 +62,52 @@ func TestWelcomeMessage(t *testing.T) {
 		t.Errorf("welcome message: got %q, want %q", got, want)
 	}
 }
+
+func TestAtoBMessage(t *testing.T) {
+	t.Parallel()
+
+	addr := ":8080"
+	m := "Hello B !"
+	want := "Hello B !"
+
+	// Setting up connA and connB
+	connA, err := net.Dial("tcp", addr)
+	if err != nil {
+		t.Fatalf("connection a failed: %v", err)
+	}
+	defer connA.Close()
+
+	connB, err := net.Dial("tcp", addr)
+	if err != nil {
+		t.Fatalf("connection b failed: %v", err)
+	}
+	defer connB.Close()
+
+	// Reading welcome message on connB and discarding it
+	var b bytes.Buffer
+	_, err = io.Copy(&b, connB)
+	if err != nil {
+		t.Fatalf("reading welcome message on connection b failed: %v", err)
+	}
+
+	b.Reset()
+
+	// Sending Message on connA
+	_, err = connA.Write([]byte(m))
+	if err != nil {
+		t.Fatalf("sending message on connection a failed: %v", err)
+	}
+
+	// Reading message on connB
+	_, err = io.Copy(&b, connB)
+	if err != nil {
+		t.Fatalf("reading message on connection b failed: %v", err)
+	}
+
+	got := b.String()
+
+	if got != want {
+		t.Errorf("connection b received message: got %s, want %s", got, want)
+	}
+
+}
