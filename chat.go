31288c415804e1d121265e3a83f91309538ab419
Thibaut Girier <30323597+tgirier@users.noreply.github.com>
2020-09-01 17:47:38 +0200

    Add username prompt

--- chat.go
+++ chat.go
@@ -17,10 +17,16 @@ type Server struct {
 	Logger         logrus.StdLogger // with standard logger can be extended with logrus
 	WelcomeMessage string
 	listener       net.Listener
-	clients        map[*net.Conn]bool
-	register       chan *net.Conn
+	clients        map[*net.Conn]string
+	register       chan *Connection
 	unregister     chan *net.Conn
 	broadcast      chan string
+}
+
+// Connection represents a user connection
+type Connection struct {
+	conn     *net.Conn
+	username string
 }
 
 // Run implements the logic handling connections
@@ -30,10 +36,10 @@ func (s *Server) Run() {
 
 	for {
 		select {
-		case conn := <-s.register:
-			s.clients[conn] = true
+		case c := <-s.register:
+			s.clients[c.conn] = c.username
 			s.Logger.Println("client connection registered")
-			go s.handle(conn)
+			go s.handle(c)
 		case msg := <-s.broadcast:
 			for conn := range s.clients {
 				_, err := fmt.Fprint(*conn, msg)
@@ -53,25 +59,45 @@ func (s *Server) listen() {
 		conn, err := s.listener.Accept()
 		if err != nil {
 			s.Logger.Println(fmt.Sprintf("connection failed: %v", err))
-			return
+			continue
 		}
-		s.register <- &conn
+
+		_, err = fmt.Fprintf(conn, s.WelcomeMessage+"\n")
+		if err != nil {
+			s.Logger.Println(fmt.Sprintf("sending message failed: %v", err))
+			continue
+		}
+
+		r := bufio.NewReader(conn)
+
+		username, err := r.ReadString('\n')
+		if err != nil {
+			s.Logger.Println(fmt.Sprintf("reading username failed: %v", err))
+			continue
+		}
+
+		c := &Connection{
+			conn:     &conn,
+			username: username,
+		}
+
+		s.register <- c
 	}
 }
 
-func (s *Server) handle(conn *net.Conn) {
-	_, err := fmt.Fprintf(*conn, s.WelcomeMessage+"\n")
-	if err != nil {
-		s.Logger.Println(fmt.Sprintf("sending message failed: %v", err))
-	}
+func (s *Server) handle(c *Connection) {
+	// _, err := fmt.Fprintf(*conn, s.WelcomeMessage+"\n")
+	// if err != nil {
+	// 	s.Logger.Println(fmt.Sprintf("sending message failed: %v", err))
+	// }
 
-	r := bufio.NewReader(*conn)
+	r := bufio.NewReader(*c.conn)
 
 	for {
 		msg, err := r.ReadString('\n')
 		if err == io.EOF {
 			s.Logger.Println("client connection closed")
-			s.unregister <- conn
+			s.unregister <- c.conn
 			break
 		}
 		if err != nil {
@@ -89,13 +115,13 @@ func (s *Server) Close() {
 // ListenAndServe blocks while the server is running
 func ListenAndServe(addr string) (err error) {
 	s := &Server{
-		WelcomeMessage: "Welcome to Chat Room!",
+		WelcomeMessage: "Welcome to Chat Room! Please enter your username:",
 	}
 
 	s.Logger = log.New(os.Stderr, "", log.LstdFlags)
-	s.register = make(chan *net.Conn, 1)
+	s.register = make(chan *Connection, 1)
 	s.unregister = make(chan *net.Conn, 1)
-	s.clients = make(map[*net.Conn]bool)
+	s.clients = make(map[*net.Conn]string)
 	s.broadcast = make(chan string, 10)
 
 	s.listener, err = net.Listen("tcp", addr)
