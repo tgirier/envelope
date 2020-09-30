82334c274fca16da13f7c5435189db92efd8c8e8
Thibaut Girier <30323597+tgirier@users.noreply.github.com>
2020-09-30 15:20:51 +0200

    Refactor unexporting server and connection

--- chat.go
+++ chat.go
@@ -13,25 +13,25 @@ import (
 	"github.com/sirupsen/logrus"
 )
 
-// Server represents a chat server
-type Server struct {
-	Logger         logrus.StdLogger
-	WelcomeMessage string
+// server represents a chat server
+type server struct {
+	logger         logrus.StdLogger
+	welcomeMessage string
 	listener       net.Listener
 	clients        map[*net.Conn]string
-	register       chan *Connection
+	register       chan *connection
 	unregister     chan *net.Conn
 	broadcast      chan string
 }
 
 // Connection represents a user connection
-type Connection struct {
+type connection struct {
 	conn     *net.Conn
 	username string
 }
 
 // Run implements the logic handling connections
-func (s *Server) run() {
+func (s *server) run() {
 
 	go s.listen()
 
@@ -39,7 +39,7 @@ func (s *Server) run() {
 		select {
 		case c := <-s.register:
 			s.clients[c.conn] = c.username
-			s.Logger.Println("client connection registered")
+			s.logger.Println("client connection registered")
 			msg := fmt.Sprintf("%s joined the chat\n", c.username)
 			s.broadcast <- msg
 			go s.handle(c)
@@ -47,27 +47,27 @@ func (s *Server) run() {
 			for conn := range s.clients {
 				_, err := fmt.Fprint(*conn, msg)
 				if err != nil {
-					s.Logger.Println(fmt.Sprintf("sending message failed: %v", err))
+					s.logger.Println(fmt.Sprintf("sending message failed: %v", err))
 				}
 			}
 		case conn := <-s.unregister:
 			delete(s.clients, conn)
-			s.Logger.Println("client connection unregistered")
+			s.logger.Println("client connection unregistered")
 		}
 	}
 }
 
-func (s *Server) listen() {
+func (s *server) listen() {
 	for {
 		conn, err := s.listener.Accept()
 		if err != nil {
-			s.Logger.Println(fmt.Sprintf("connection failed: %v", err))
+			s.logger.Println(fmt.Sprintf("connection failed: %v", err))
 			continue
 		}
 
-		_, err = fmt.Fprintf(conn, s.WelcomeMessage+"\n")
+		_, err = fmt.Fprintf(conn, s.welcomeMessage+"\n")
 		if err != nil {
-			s.Logger.Println(fmt.Sprintf("sending message failed: %v", err))
+			s.logger.Println(fmt.Sprintf("sending message failed: %v", err))
 			continue
 		}
 
@@ -75,13 +75,13 @@ func (s *Server) listen() {
 
 		username, err := r.ReadString('\n')
 		if err != nil {
-			s.Logger.Println(fmt.Sprintf("reading username failed: %v", err))
+			s.logger.Println(fmt.Sprintf("reading username failed: %v", err))
 			continue
 		}
 
 		username = strings.TrimSuffix(username, "\n")
 
-		c := &Connection{
+		c := &connection{
 			conn:     &conn,
 			username: username,
 		}
@@ -90,29 +90,24 @@ func (s *Server) listen() {
 	}
 }
 
-func (s *Server) handle(c *Connection) {
+func (s *server) handle(c *connection) {
 	r := bufio.NewReader(*c.conn)
 
 	for {
 		msg, err := r.ReadString('\n')
 		if err == io.EOF {
-			s.Logger.Println("client connection closed")
+			s.logger.Println("client connection closed")
 			s.unregister <- c.conn
 			break
 		}
 		if err != nil {
-			s.Logger.Println(fmt.Sprintf("receiving message failed: %v", err))
+			s.logger.Println(fmt.Sprintf("receiving message failed: %v", err))
 		}
 
 		msg = c.username + ": " + msg
 
 		s.broadcast <- msg
 	}
-}
-
-// Close closes all connection to the server
-func (s *Server) Close() {
-	s.listener.Close()
 }
 
 // ListenAndServe blocks while the server is running
@@ -123,12 +118,12 @@ func ListenAndServe(addr string) (err error) {
 // ListenAndServeWithLogger blocks while the server is running.
 // Enable logger customization
 func ListenAndServeWithLogger(addr string, logger logrus.StdLogger) (err error) {
-	s := &Server{
-		WelcomeMessage: "Welcome to Chat Room! Please enter your username:",
+	s := &server{
+		welcomeMessage: "Welcome to Chat Room! Please enter your username:",
 	}
 
-	s.Logger = logger
-	s.register = make(chan *Connection, 1)
+	s.logger = logger
+	s.register = make(chan *connection, 1)
 	s.unregister = make(chan *net.Conn, 1)
 	s.clients = make(map[*net.Conn]string)
 	s.broadcast = make(chan string, 10)
@@ -139,7 +134,7 @@ func ListenAndServeWithLogger(addr string, logger logrus.StdLogger) (err error) 
 		return err
 	}
 
-	s.Logger.Println(fmt.Sprintf("Listening on %v", addr))
+	s.logger.Println(fmt.Sprintf("Listening on %v", addr))
 
 	s.run()
 
